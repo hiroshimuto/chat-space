a25a573cedb78f7e6e4586a6284f4deec9e72674
hiroshimuto <hiroshi.github@gmail.com>
2018-11-14 14:32:18 +0900

    Create group create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,18 @@
 class GroupsController < ApplicationController
   def new
+    @group = Group.new
+    @group.users << current_user
+    # ログイン中のユーザーを新規作成グループに追加
 
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: "グループを作成しました。"
+    else
+      render :new
+    end
 
   end
 
@@ -13,5 +22,10 @@ class GroupsController < ApplicationController
 
   def update
 
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, {:user_ids => []})
   end
 end
