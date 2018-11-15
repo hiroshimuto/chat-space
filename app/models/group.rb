97e7705d964c64c5988dccb17062c9244848556a
hiroshimuto <hiroshi.github@gmail.com>
2018-11-15 15:14:21 +0900

    Update view of message

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,15 @@ class Group < ApplicationRecord
   has_many :users, through: :members
   validates :name, presence: true
   has_many :messages
+
+  # ビューのif文を書くと煩雑になるため、モデルにインスタンスメソッドを定義することでシンプルにする。
+  def show_last_message
+    # 最後の投稿されたメッセージ.lastメソッドで代入をし、presentメソッドで判断
+    if (last_message = messages.last).present?
+      # 三項演算子で出し分ける
+      last_message.body? ? last_message.body : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
 end
